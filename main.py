898227c14c61e88ebf150b6c3f8671cd072fe9d0
crisleo94 <cristhian.reinoso@live.com>
2021-09-07 19:55:58 -0500

    create basic ttk structure

--- main.py
+++ main.py
@@ -1,100 +1,65 @@
 import ctypes
-import time
 from ctypes import wintypes
-
-from simple_chalk import chalk
+from tkinter import Tk, ttk
 
 user32 = ctypes.windll.user32
+root = Tk()
 
-EnumWindows = ctypes.windll.user32.EnumWindows
+EnumWindows = user32.EnumWindows
 EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(
-    ctypes.c_int),     ctypes.POINTER(ctypes.c_int))
-GetWindowText = ctypes.windll.user32.GetWindowTextW
-GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
-IsWindowVisible = ctypes.windll.user32.IsWindowVisible
-rect = wintypes.RECT()
+    ctypes.c_int), ctypes.POINTER(ctypes.c_int))
 
-titles = []
-matched_titles = []
-terminal = '@crist'  # if you have ur powershell profile modified be sure to include the username here, otherwise just powershell
-navigator = 'Mozilla Firefox'  # the browser you want to match
-editor = 'Visual Studio Code'  # the editor/ide you want to match
-matched_terminal = ''
-matched_navigator = ''
-matched_editor = ''
+win_titles = []
+win_handles = []
+win_coords = []
+
+
+def main_layout():
+    root.title('Window Reminder')
+    root.geometry('500x500')
+    ttk.Button(root, text='Find Windows', command=show,
+               width=50, padding=2).grid()
+    ttk.Labelframe()
+    root.mainloop()
+
+
+def show():
+    print('Button clicked!!')
 
 
 def foreach_window(hwnd, lParam):
-
-    if IsWindowVisible(hwnd):
-        length = GetWindowTextLength(hwnd)
+    if user32.IsWindowVisible(hwnd):
+        length = user32.GetWindowTextLengthW(hwnd)
         buff = ctypes.create_unicode_buffer(length + 1)
-        GetWindowText(hwnd, buff, length + 1)
-        if navigator in buff.value or editor in buff.value or terminal in buff.value:
-            matched_titles.append(buff.value)
-        titles.append(buff.value)
-    return titles
+        user32.GetWindowTextW(hwnd, buff, length + 1)
+        if buff.value != '':
+            win_titles.append(buff.value)
+            print(buff.value)
+    return win_titles
 
 
-EnumWindows(EnumWindowsProc(foreach_window), 0)
+def find_window(window_list):
+    for window in window_list:
+        found_window = user32.FindWindowW(None, window)
+        user32.ShowWindow(found_window, 9)
 
-for title in matched_titles:
-    if terminal in title:
-        matched_terminal = title
-    elif navigator in title:
-        matched_navigator = title
-    elif editor in title:
-        matched_editor = title
 
-time.sleep(1)
-print(chalk.green(f'Terminal value found: {matched_terminal}'))
-time.sleep(1)
-print(chalk.green(f'Navigator value found: {matched_navigator}'))
-time.sleep(1)
-print(chalk.green(f'Editor value found: {matched_editor}'))
-time.sleep(1)
+def find_window_location(window_list):
+    rect = wintypes.RECT()
+    for window in window_list:
+        window_position = user32.GetWindowRect(window, ctypes.pointer(rect))
+    return window_position.left, window_position.right, window_position.top, window_position.bottom
 
-# get handle for Notepad window
 # non-zero value for handle should mean it found a window that matches
-wterminal = user32.FindWindowW(None, matched_terminal)
-wnavigator = user32.FindWindowW(None, matched_navigator)
-weditor = user32.FindWindowW(None, matched_editor)
-time.sleep(2)
-ff = user32.GetWindowRect(wterminal, ctypes.pointer(rect))
-print(chalk.blue(
-    f'Coords for terminal, X:{rect.left}, Y: {rect.top}, HEIGHT: {rect.right}, WIDTH: {rect.bottom}'))
+# Bring window to front
 
-time.sleep(2)
-ff2 = user32.GetWindowRect(wnavigator, ctypes.pointer(rect))
-print(chalk.blue(
-    f'Coords for Browser, X:{rect.left}, Y: {rect.top}, HEIGHT: {rect.right}, WIDTH: {rect.bottom}'))
-
-time.sleep(2)
-ff3 = user32.GetWindowRect(weditor, ctypes.pointer(rect))
-print(chalk.blue(
-    f'Coords for Editor, X:{rect.left}, Y: {rect.top}, HEIGHT: {rect.right}, WIDTH: {rect.bottom}'))
-
-# meaning of 2nd parameter defined here
-# https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
-# minimize window using handle
-# user32.ShowWindow(handle, 6)
-# maximize window using handle
-# user32.ShowWindow(handle, 9)
+# Get info with rect(rect.left, rect.right, rect.top, rect.bottom)
 
 # move window using handle
 # MoveWindow(handle, x, y, height, width, repaint(bool))
-if wterminal and wnavigator and weditor != 0:
-    time.sleep(2)
-    print(chalk.yellowBright('Moving window...'))
-    time.sleep(2)
-    user32.MoveWindow(wterminal, -5, 713, 972, 340, True)
-    print(chalk.yellowBright('Moving window...'))
-    time.sleep(2)
-    user32.MoveWindow(wnavigator, 955, 0, 960, 1045, True)
-    print(chalk.yellowBright('Moving window...'))
-    time.sleep(2)
-    user32.MoveWindow(weditor, 0, 0, 963, 714, True)
-    time.sleep(2)
-    print(chalk.green('Specified windows have been moved!'))
-else:
-    print(chalk.red('No windows found!'))
+
+
+if __name__ == '__main__':
+    print('Running...')
+    EnumWindows(EnumWindowsProc(foreach_window), 0)
+    main_layout()
